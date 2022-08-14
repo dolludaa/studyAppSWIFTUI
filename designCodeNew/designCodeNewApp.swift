a31be525b178f7913f3a6b0e09d89a109d1dbe4e
dolludaa <dolontaeva00@gmail.com>
2022-08-14 14:46:40 +0400

    Initial Commit

---
+++ designCodeNew/designCodeNewApp.swift
@@ -0,0 +1,17 @@
+//
+//  designCodeNewApp.swift
+//  designCodeNew
+//
+//  Created by Людмила Долонтаева on 14.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct designCodeNewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
